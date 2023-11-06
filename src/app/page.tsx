2b1d8c989fbb88eead4a94c74278b63ee2b8c5cc
Bruno Filho <bruno@publi.com.br>
2023-11-06 16:54:39 -0300

    home changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import { Product } from '@/model/products'
 import { useQuery } from '@tanstack/react-query'
 import Link from 'next/link'
 import { useState } from 'react'
+import { motion } from 'framer-motion';
 
 export default function Home() {
   const [dialogOpen, setDialogOpen] = useState(false)
@@ -39,6 +40,32 @@ export default function Home() {
   return (
     <div className="flex container min-h-screen flex-col items-center justify-between gap-8">
       <Header />
+
+      <motion.div 
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.5 }}
+      >
+      <section className='flex items-start w-full h-full gap-2'>
+        <div className='flex flex-col justify-between space-y-4'>
+          <div>
+            <h1 className='w-fit rounded-2xl text-8xl font-extrabold px-6 bg-slate-800'>NEW</h1>            
+            <h1 className='w-fit rounded-2xl text-8xl font-extrabold px-6 bg-yellow-500'>ARRIVAL</h1>                        
+          </div>
+          <p className='text-muted-foreground'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium explicabo vitae dolorem voluptates cumque suscipit ea ex, tempora quibusdam, provident incidunt tenetur at fugit accusantium? Ea voluptas obcaecati hic sapiente.</p>
+
+          <h3 className='text-4xl font-extrabold'>Monday, 29th November 2077</h3>
+
+          <div className='flex gap-2'>
+            <p className='font-bold text-muted-foreground'>@brunofilho1</p>
+            <p className='font-bold text-muted-foreground'>@ecommerceexperience</p>
+          </div>
+        </div>
+
+        <img src='/home-pic.jpg' alt="Home Pic" className='rounded-2xl border-r-8 border-b-4 border-b-yellow-600 border-r-yellow-700' />
+      </section>
+      </motion.div>
 
       <section className="flex flex-col gap-4">
         <h1 className='font-extrabold text-xl'>See Our Products</h1>
